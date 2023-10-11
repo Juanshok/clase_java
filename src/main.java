416abf3966bcd1b61067cb8ecea60f6761636f18
juan.ruiz2 <juan.ruiz@sqasa.co>
2023-10-10 21:45:03 -0500

    Definicion y toma de variables

---
+++ src/main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args){
+
+        //Definicion de variables
+        String nombre = "Juan";
+        byte llave = 1;
+        int edad = 0;
+        long cedula1 = 15;
+        float resultado = 10.1F;
+        double resultado2 = 1.025;
+        String cedula = "545565454asd5";
+        boolean resultado4 = false;
+
+
+        // Impresiones
+        System.out.println(edad + " Juasdasd " + nombre);
+        System.out.println(nombre);
+
+
+
+        // Toma de datos por consola
+        double numero1 = 12;
+        double numero2 = 23;
+        double resul = numero1 * numero2;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero ");
+        numero1 = teclado.nextDouble();
+        System.out.println("El Primer numero es: " + numero1);
+
+
+
+
+
+
+
+
+
+
+    }
+}
